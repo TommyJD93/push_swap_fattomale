a6b09c1a4455c1cf7444429dd720666f09a1cda9
Tommaso <tommy.terribili@gmail.com>
2022-02-24 08:18:00 +0100

    updated moves

--- moves1.c
+++ moves1.c
@@ -6,7 +6,7 @@
 /*   By: tterribi <tterribi@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 11:27:29 by tterribi          #+#    #+#             */
-/*   Updated: 2022/02/22 17:34:04 by tterribi         ###   ########.fr       */
+/*   Updated: 2022/02/24 08:16:33 by tterribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int	*rotate_a(int *stack_a)
 	int	i;
 
 	len = length(stack_a);
-	tmp = (int*)malloc(sizeof(int*) * len);
+	tmp = (int *)malloc(sizeof(int *) * len);
 	i = 0;
 	while (i < len - 1)
 	{
-		tmp[i] = stack_a[i+1];
+		tmp[i] = stack_a[i + 1];
 		i++;
 	}
-	tmp[len-1] = stack_a[0];
+	tmp[len - 1] = stack_a[0];
 	tmp[len] = '\0';
 	free(stack_a);
-	return(tmp);
+	return (tmp);
 }
 
 int	*rotate_b(int *stack_b)
@@ -39,19 +39,58 @@ int	*rotate_b(int *stack_b)
 	int	i;
 
 	len = length(stack_b);
-	tmp = (int*)malloc(sizeof(int*) * len);
+	tmp = (int *)malloc(sizeof(int *) * len);
 	i = 0;
 	while (i < len - 1)
 	{
-		tmp[i] = stack_b[i+1];
+		tmp[i] = stack_b[i + 1];
 		i++;
 	}
-	tmp[len-1] = stack_b[0];
+	tmp[len - 1] = stack_b[0];
 	tmp[len] = '\0';
 	free(stack_b);
-	return(tmp);
+	return (tmp);
 }
 
+int	*reverse_rotate_a(int *stack_a)
+{
+	int	*tmp;
+	int	len;
+	int	i;
+
+	len = length(stack_a);
+	tmp = (int *)malloc(sizeof(int *) * len);
+	i = 0;
+	while (i < len - 1)
+	{
+		tmp[i+1] = stack_a[i];
+		i++;
+	}
+	tmp[0] = stack_a[len-1];
+	tmp[len] = '\0';
+	free(stack_a);
+	return (tmp);
+}
+
+int *reverse_rotate_b(int *stack_b)
+{
+	int *tmp;
+	int len;
+	int i;
+
+	len = length(stack_b);
+	tmp = (int *)malloc(sizeof(int *) * len);
+	i = 0;
+	while (i < len - 1)
+	{
+		tmp[i + 1] = stack_b[i];
+		i++;
+	}
+	tmp[0] = stack_b[len - 1];
+	tmp[len] = '\0';
+	free(stack_b);
+	return (tmp);
+}
 /*
 int main(void)
 {
@@ -68,7 +107,7 @@ int main(void)
 	}
 	asd[i] = '\0';
 	//write(1, "a\n", 2);
-	asd1 = rotate_b(asd);
+	asd1 = reverse_rotate_b(asd);
 	//write(1, "c\n", 2);
 	i = 0;
 //	write(1, "b\n", 2);
